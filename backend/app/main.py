62b6f6353e0d6dc7a6ec5b00a5ed8bd57ecb5e60
Vishwas Charan <vishwascharan11@gmail.com>
2026-03-25 17:09:51 +0530

    added vite deployed url

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://interview-scheduling-chatbot.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
